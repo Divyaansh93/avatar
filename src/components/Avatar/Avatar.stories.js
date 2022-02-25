0b610c3937b6686c4ae4bb50da725ebea43fff8e
Divyaansh Jaiswal <divyaansh@saal.ai>
2022-02-25 08:57:36 +0530

    Update Avatar

---
+++ src/components/Avatar/Avatar.stories.js
@@ -0,0 +1,17 @@
+// src/components/Avatar.stories.js
+import React from 'react';
+import Avatar from './Avatar';
+
+
+export default {
+  title: 'Example/Avatar',
+  component: Avatar,
+  argTypes: {},
+};
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Hello'
+};
